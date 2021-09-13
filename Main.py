a1687ebd82ef02b31ed1b988fa32201b84f3fb9e
Penders5 <bigdlego.dp@gmail.com>
2021-09-13 09:38:41 -0500

    Top 5 Movies

---
+++ Main.py
@@ -0,0 +1,17 @@
+#print Top 5 Video Games
+print("Top 5 Video Games")
+
+#Print 1st Game
+print("1: Destiny 2")
+
+#Print 2nd Game
+print("2: Hollow Knight")
+
+#Print 3rd Game
+print("3: Portal 2")
+
+#Print 4th Game
+print("4: The Bindog If Isaac: Repentance")
+
+#Print 5th Game
+print("5: Tom Clancy's Rainbow: Six Siege")
