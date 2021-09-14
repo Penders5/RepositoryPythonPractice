4fb462f6e6dc33ba359fbda1c5e83313441c4537
Penders5 <bigdlego.dp@gmail.com>
2021-09-14 09:34:14 -0500

    Defined "games"

---
+++ def.py
@@ -0,0 +1,24 @@
+def games():
+    #print Top 5 Video Games
+    print("Top 5 Video Games")
+
+    #Print 1st Game
+    print("1: Destiny 2")
+
+    #Print 2nd Game
+    print("2: Hollow Knight")
+
+    #Print 3rd Game
+    print("3: Portal 2")
+
+    #Print 4th Game
+    print("4: The Bindog If Isaac: Repentance")
+
+    #Print 5th Game
+    print("5: Tom Clancy's Rainbow: Six Siege")
+
+#Print top 5 games
+print("these are my top five video games")
+
+#Run function games
+games()
