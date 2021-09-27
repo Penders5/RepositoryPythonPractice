41213015e3f658d86a0f88e1210f92ddfeafb0e3
Penders5 <bigdlego.dp@gmail.com>
2021-09-27 09:03:53 -0500

    made something

--- Main.py
+++ Main.py
@@ -1,17 +1,11 @@
-#print Top 5 Video Games
-print("Top 5 Video Games")
+def highLow(x):
 
-#Print 1st Game
-print("1: Destiny 2")
+    if x > 10:
+        return "High!"
+    elif x == 10:
+        return "Good ammount"
+    else: 
+        return "Low..."
 
-#Print 2nd Game
-print("2: Hollow Knight")
 
-#Print 3rd Game
-print("3: Portal 2")
-
-#Print 4th Game
-print("4: The Bindog If Isaac: Repentance")
-
-#Print 5th Game
-print("5: Tom Clancy's Rainbow: Six Siege")
+print(highLow(10))
