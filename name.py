e231aa656a2cf9b2137e49927c7afe7972f014db
Penders5 <bigdlego.dp@gmail.com>
2021-10-01 09:17:39 -0500

    :P

--- name.py
+++ name.py
@@ -1,7 +1,11 @@
-#define name
-def name(name):
-    
-    #output string
-    print(name + " is my name.")
+def dog():
 
-name("Cayde-6")
+    c = 1
+    x = 1
+    while x <= 10:
+        while c <= 10:
+            print(c)
+            c = c + 1
+        x = x + 1
+        c = 1
+dog()
